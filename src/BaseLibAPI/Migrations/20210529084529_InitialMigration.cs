993f061605aaad5ed426e62e4c013673f561a22f
Mastewal B <se.mastewal.belete@gmail.com>
2021-05-29 12:05:39 +0300

    Migrations Added

---
+++ src/BaseLibAPI/Migrations/20210529084529_InitialMigration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BaseLibAPI.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ISBN = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Publisher = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PublishDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Edition = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "Description", "Edition", "ISBN", "PublishDate", "Publisher", "Title" },
+                values: new object[] { new Guid("d28888e9-2ba9-473a-a40f-e38cb54f9b35"), "Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein", "Before there were computers, there were algorithms. But now that there are com-puters, there are even more algorithms, and algorithms lie at the heart of computing.", 3, "9780262033848", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified).AddTicks(2009), "The MIT Press", "Introduction to Algorithms" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
