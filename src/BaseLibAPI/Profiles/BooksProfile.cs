a6f92134af8c6b148639b45b6309685cd878058b
Mastewal B <se.mastewal.belete@gmail.com>
2021-05-29 19:44:40 +0300

    Mapping Profile from Book Model to BookDto

---
+++ src/BaseLibAPI/Profiles/BooksProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace BaseLibAPI.Profiles
+{
+    public class BooksProfile : Profile
+    {
+        public BooksProfile()
+        {
+            CreateMap<Models.Book, ModelDTOs.BookDto>();
+
+        }
+    }
+}
