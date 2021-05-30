08429877ced940f7ab6bd95819fdd889c3d188a3
Mastewal B <se.mastewal.belete@gmail.com>
2021-05-30 19:30:00 +0300

    Course and BookResource added to DbContext

--- src/BaseLibAPI/DbContexts/BaseLibContext.cs
+++ src/BaseLibAPI/DbContexts/BaseLibContext.cs
@@ -12,22 +12,9 @@ namespace BaseLibAPI.DbContexts
         public BaseLibContext(DbContextOptions<BaseLibContext> options) : base(options) { }
 
         public DbSet<Book> Books { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<BookResourse> BookResourses { get; set; }
         
-        protected override void OnModelCreating (ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Book>().HasData(
-                new Book()
-                {
-                    Id = Guid.Parse("d28888e9-2ba9-473a-a40f-e38cb54f9b35"),
-                    Title = "Introduction to Algorithms",
-                    Author = "Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein",
-                    ISBN = "9780262033848",
-                    Publisher = "The MIT Press",
-                    PublishDate = new DateTime(2009),
-                    Edition = 3,
-                    Description = "Before there were computers, there were algorithms. But now that there are com-puters, there are even more algorithms, and algorithms lie at the heart of computing.",
-                }
-                );
-        }
+        
     }
 }
