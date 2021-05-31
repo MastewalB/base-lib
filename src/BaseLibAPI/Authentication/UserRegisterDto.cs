e27c1879be1cb407bb4c4d84c31dac80390ee992
Mastewal B <se.mastewal.belete@gmail.com>
2021-05-31 22:37:10 +0300

    Data Transfer Object for User

---
+++ src/BaseLibAPI/Authentication/UserRegisterDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BaseLibAPI.Authentication
+{
+    public class UserRegisterDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "User Name is required")]
+        public string Username { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
