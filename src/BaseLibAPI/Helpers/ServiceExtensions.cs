4e124c76aab7da8fb323a4ca0f0ccd989ffe071e
Mastewal B <se.mastewal.belete@gmail.com>
2021-05-31 22:38:28 +0300

    Additional Helper Class to Build User Identity

---
+++ src/BaseLibAPI/Helpers/ServiceExtensions.cs
@@ -0,0 +1,40 @@
+﻿using BaseLibAPI.Authentication;
+using BaseLibAPI.DbContexts;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaseLibAPI.Helpers
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureIdentity(this IServiceCollection services)
+        {
+            var builder = services.AddIdentityCore<User>(o =>
+            {
+                o.Password.RequireDigit = true;
+                o.Password.RequireLowercase = false;
+                o.Password.RequireUppercase = false;
+                o.Password.RequireNonAlphanumeric = false;
+                o.Password.RequiredLength = 10;
+                o.User.RequireUniqueEmail = true;
+            });
+
+            builder = new IdentityBuilder(builder.UserType, typeof(IdentityRole),
+                builder.Services);
+            builder.AddEntityFrameworkStores<BaseLibContext>()
+                .AddDefaultTokenProviders();
+        }
+    }
+}
+
+
+/*
+ * IdentityUser in User.cs and UserRegisterDto.cs 
+ * ServiceExtensions => ConfigureIdentity
+ * StartUp.cs => configure - useAuthentication()
+ * StartUp.cs => configureServices - services.ConfigureIdentity()
+ */
