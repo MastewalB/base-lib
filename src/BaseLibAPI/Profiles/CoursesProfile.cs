179871de72d095adde1819560e6104a0e3904ec9
Mastewal B <se.mastewal.belete@gmail.com>
2021-05-31 08:27:10 +0300

    Course Profile for CourseDto

---
+++ src/BaseLibAPI/Profiles/CoursesProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using BaseLibAPI.Helpers;
+
+namespace BaseLibAPI.Profiles
+{
+    public class CoursesProfile : Profile
+    {
+        public CoursesProfile()
+        {
+            CreateMap<Models.Course, ModelDTOs.CourseReadDto>()
+                /*.ForMember(
+                dest => dest.BookResources,
+                opt => opt.MapFrom(src => src.Id.GetCourseResources()));*/
+                ;
+        }
+    }
+}
