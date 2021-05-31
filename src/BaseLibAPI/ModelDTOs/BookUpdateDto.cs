0da29c42314065045b8a22005fd16ecdf5923dea
Mastewal B <se.mastewal.belete@gmail.com>
2021-05-31 12:39:06 +0300

    Update Data Transfer Object for Book and Course

---
+++ src/BaseLibAPI/ModelDTOs/BookUpdateDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BaseLibAPI.ModelDTOs
+{
+    public class BookUpdateDto
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        public string ISBN { get; set; }
+
+        public string Publisher { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public int Edition { get; set; }
+
+        public string Description { get; set; }
+        public string CoverImg { get; set; }
+        public string SkillLevel { get; set; }
+    }
+}
