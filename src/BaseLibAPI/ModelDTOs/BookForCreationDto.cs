ff0a754d44f38aa0369ea13d146092406abf2899
Mastewal B <se.mastewal.belete@gmail.com>
2021-05-31 09:29:11 +0300

    Yet another Dto for Create Course and Book

--- src/BaseLibAPI/ModelDTOs/BookForCreationDto.cs
+++ src/BaseLibAPI/ModelDTOs/BookForCreationDto.cs
@@ -2,10 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace BaseLibAPI.ModelDTOs
 {
     public class BookForCreationDto
     {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        public string ISBN { get; set; }
+
+        public string Publisher { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public int Edition { get; set; }
+
+        public string Description { get; set; }
+        public string CoverImg { get; set; }
+        public string SkillLevel { get; set; }
     }
 }
