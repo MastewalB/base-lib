76a061e4be5d75d54ceaad1eacd2970ca907afb8
Mastewal B <se.mastewal.belete@gmail.com>
2021-05-31 08:28:21 +0300

    Helper Methods for Course Resources

---
+++ src/BaseLibAPI/Helpers/CourseResourceExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BaseLibAPI.Models;
+
+namespace BaseLibAPI.Helpers
+{
+    public static class CourseResourceExtensions
+    {
+        public static void GetCourseResources(this int Id)
+        {
+            
+        }
+    }
+}
