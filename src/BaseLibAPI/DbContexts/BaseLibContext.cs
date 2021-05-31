21cb66d672d2bfa267303875c8f23112a5f81257
Mastewal B <se.mastewal.belete@gmail.com>
2021-05-31 22:37:49 +0300

    DbContext upgraded to support Identity

--- src/BaseLibAPI/DbContexts/BaseLibContext.cs
+++ src/BaseLibAPI/DbContexts/BaseLibContext.cs
@@ -1,4 +1,6 @@
-﻿using BaseLibAPI.Models;
+﻿using BaseLibAPI.Authentication;
+using BaseLibAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -7,14 +9,17 @@ using System.Threading.Tasks;
 
 namespace BaseLibAPI.DbContexts
 {
-    public class BaseLibContext:DbContext
+    public class BaseLibContext:IdentityDbContext<User>
     {
         public BaseLibContext(DbContextOptions<BaseLibContext> options) : base(options) { }
 
         public DbSet<Book> Books { get; set; }
         public DbSet<Course> Courses { get; set; }
         public DbSet<BookResourse> BookResourses { get; set; }
-        
-        
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
 }
