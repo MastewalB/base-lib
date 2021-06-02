3388e2f12ca905f0c9d9f8502055ebf0cdf68d41
Mastewal B <se.mastewal.belete@gmail.com>
2021-06-02 09:56:42 +0300

    AuthenticationManagement Interface

---
+++ src/BaseLibAPI/Authentication/IAuthenticationManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BaseLibAPI.ModelDTOs;
+
+namespace BaseLibAPI.Authentication
+{
+    public interface IAuthenticationManager
+    {
+        Task<bool> ValidateUser(UserLoginDto userLoginDto);
+        Task<string> CreateToken();
+    }
+}
