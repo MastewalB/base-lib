2b6295bc7543b62030571fe590b04f7ed94b0e3b
Mastewal B <se.mastewal.belete@gmail.com>
2021-06-02 04:42:50 +0300

    RegisterUser Method

---
+++ src/BaseLibAPI/Controllers/AuthenticationController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using BaseLibAPI.Authentication;
+using BaseLibAPI.ModelDTOs;
+
+namespace BaseLibAPI.Controllers
+{
+    [Route("api/authentication")]
+    [ApiController]
+    public class AuthenticationController:ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
+
+        public AuthenticationController(IMapper mapper, UserManager<User> userManager)
+        {
+            _mapper = mapper;
+            _userManager = userManager; 
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterUser([FromBody] UserRegisterDto userRegisterDto)
+        {
+            var user = _mapper.Map<User>(userRegisterDto);
+
+            var result = await _userManager.CreateAsync(user, userRegisterDto.Password);
+            if (!result.Succeeded)
+            {
+                foreach( var error in result.Errors)
+                {
+                    ModelState.TryAddModelError(error.Code, error.Description);
+                }
+                return BadRequest(ModelState);
+            }
+            await _userManager.AddToRoleAsync(user, userRegisterDto.Roles);
+            return Ok();
+        }
+    }
+}
