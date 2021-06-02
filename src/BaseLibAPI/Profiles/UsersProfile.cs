6beadb59476cd14dc09e0e5c20643c56302d343f
Mastewal B <se.mastewal.belete@gmail.com>
2021-06-02 04:43:17 +0300

    Mapper Profile for  UserRegisterDto and User

---
+++ src/BaseLibAPI/Profiles/UsersProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using BaseLibAPI.Authentication;
+using BaseLibAPI.ModelDTOs;
+
+namespace BaseLibAPI.Profiles
+{
+    public class UsersProfile:Profile
+    {
+        public UsersProfile()
+        {
+            CreateMap<UserRegisterDto, User>();
+        }
+    }
+}
