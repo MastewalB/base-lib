ceef57bb2bcd482d34ed06db4b3de25fe524679c
Mastewal B <se.mastewal.belete@gmail.com>
2021-06-02 09:58:31 +0300

    JWT Configuration Method

--- src/BaseLibAPI/Helpers/ServiceExtensions.cs
+++ src/BaseLibAPI/Helpers/ServiceExtensions.cs
@@ -1,11 +1,15 @@
 ﻿using BaseLibAPI.Authentication;
 using BaseLibAPI.DbContexts;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text;
 
 namespace BaseLibAPI.Helpers
 {
@@ -28,6 +32,33 @@ namespace BaseLibAPI.Helpers
             builder.AddEntityFrameworkStores<BaseLibContext>()
                 .AddDefaultTokenProviders();
         }
+
+        public static void ConfigureJWT(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtSettings = configuration.GetSection("JwtSettings");
+            var secretKey = Environment.GetEnvironmentVariable("SECRET");
+
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+              .AddJwtBearer(options =>
+              {
+                  options.TokenValidationParameters = new TokenValidationParameters
+                  {
+                      ValidateIssuer = true,
+                      ValidateAudience = true,
+                      ValidateLifetime = true,
+                      ValidateIssuerSigningKey = true,
+
+                      ValidIssuer = jwtSettings.GetSection("validIssuer").Value,
+                      ValidAudience = jwtSettings.GetSection("validAudience").Value,
+                      IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
+                  };
+              });
+        }
+        
     }
 }
 
