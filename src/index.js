64150b04fea04bfeba0e8f30f29b687b974a0503
Meti A <metiade7@gmail.com>
2021-07-15 05:16:24 +0300

    Theme added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { createBrowserHistory } from 'history';
 import { Router, Route, Switch } from 'react-router-dom';
 import Navbar from 'components/NavBar/navbar.js';
-
+import './index.css';
 import 'assets/scss/material-kit-react.scss?v=1.10.0';
 
 // pages for this product
@@ -13,20 +13,23 @@ import SignupPage from 'views/SignupPage/SignupPage.js';
 import Courses from 'views/Courses/Courses.js';
 import Books from 'views/Book/Book.js';
 import Admin from 'views/Admin/Admin.js';
-
+import theme from './theme';
+import { ThemeProvider } from '@material-ui/core';
 var hist = createBrowserHistory();
 
 ReactDOM.render(
-	<Router history={hist}>
-		<Navbar />
-		<Switch>
-			<Route path="/login-page" component={LoginPage} />
-			<Route path="/signup-page" component={SignupPage} />
-			<Route path="/courses" component={Courses} />
-			<Route path="/books/:id" component={Books} />
-			<Route path="/admin" component={Admin} />
-			<Route path="/" component={Components} />
-		</Switch>
-	</Router>,
+	<ThemeProvider theme={theme}>
+		<Router history={hist}>
+			<Navbar />
+			<Switch>
+				<Route path="/login-page" component={LoginPage} />
+				<Route path="/signup-page" component={SignupPage} />
+				<Route path="/courses" component={Courses} />
+				<Route path="/books/:id" component={Books} />
+				<Route path="/admin" component={Admin} />
+				<Route path="/" component={Components} />
+			</Switch>
+		</Router>
+	</ThemeProvider>,
 	document.getElementById('root')
 );
