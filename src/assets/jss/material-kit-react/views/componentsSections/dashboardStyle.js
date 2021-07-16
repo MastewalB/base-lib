d6adafeae0c3b4889603cff2777ef5109f823be1
Meti A <metiade7@gmail.com>
2021-07-16 04:35:25 +0300

    styling for dashboard components

---
+++ src/assets/jss/material-kit-react/views/componentsSections/dashboardStyle.js
@@ -0,0 +1,41 @@
+import { container } from "assets/jss/material-kit-react.js";
+
+const tabsStyle = {
+  section: {
+    background: "#EEEEEE",
+    padding: "20px 0",
+  },
+  danger :{
+    background: "#FF006E",
+    
+  },
+  blueAvatar :{
+      background:"#2E86AB"
+  },
+  dashboardAvatar : {
+      background:"#4C243B"
+  },
+  warning :{
+    background: "#FC6722"
+  },
+  successText :{
+    color : "#058E3F",
+    fontWeight: 900,
+    fontSize : "30em"
+  },
+  successAvatar :{
+    background: "#058E3F",
+  },
+  success :{
+    color: "#058E3F",
+  },
+  container,
+  textCenter: {
+    textAlign: "center",
+  },
+  media: {
+    height: 250,
+  },
+};
+
+export default tabsStyle;
